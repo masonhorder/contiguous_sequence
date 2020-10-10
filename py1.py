e7ed71806ca68a72f57971c2002522ce501b20a2
masonhorder <masonhorder@gmail.com>
2020-10-09 20:52:30 -0700

    python first solution

--- py1.py
+++ py1.py
@@ -1,1 +1,19 @@
-print("hi")
+import time
+input = [-2, -3, 4, -1, -2, 1, 5, -3]
+print(input)
+
+i = 0
+max = input[0]
+while i < len(input): 
+    # print(input[i], i)
+    a = len(input)-i
+    b = 0
+    d = 0
+    while a > b:
+        c = b + i
+        b+=1
+        d+=input[c]
+        if d > max:
+            max = d
+    i += 1
+print(max)
