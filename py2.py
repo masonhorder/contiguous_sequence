35fbbd00edc5df60cc997e02c35a49fe830ab982
masonhorder <masonhorder@gmail.com>
2020-10-09 21:21:00 -0700

    updates

--- py2.py
+++ py2.py
@@ -2,9 +2,11 @@ from datetime import datetime
 startTime = datetime.now()
 
 input = [-2, -3, 4, -1, -2, 1, 5, -3]
+
 i = 0
 max = input[0]
-while i < len(input): 
+for item in input: 
+    # print(input[i], i)
     a = len(input)-i
     b = 0
     d = 0
@@ -15,7 +17,6 @@ while i < len(input):
         if d > max:
             max = d
     i += 1
-
 print(max)
 
 print(datetime.now() - startTime)
